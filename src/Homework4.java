1d9b5e2241ab130d94f6cb1800bb6918ad6149f4
Haldlugal <haldlugal@mail.ru>
2022-06-08 21:13:38 +0700

    hw 4

--- src/Homework4.java
+++ src/Homework4.java
@@ -3,7 +3,7 @@
 public class Homework4 {
 
     public static void main(String[] args) {
-        Queue queue = new Queue();
+        LinkedQueue queue = new LinkedQueue();
         queue.insert(1);
         queue.insert(3);
         queue.insert(2);
